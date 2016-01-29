d371195beecf97b6a30356ef5aceaaca7ef90e92
Chris Grimmett <chris@grimtech.net>
2016-01-28 19:22:33 -0800

    try docker entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM node:4-onbuild
 MAINTAINER Chris Grimmett <chris@grimtech.net>
 EXPOSE 3000
+
+ENTRYPOINT ["npm"]
+CMD ["start"]
--- index.js
+++ index.js
@@ -2,8 +2,5 @@ var path = require('path');
 var server = require(path.join(__dirname, 'server'));
 var api = require(path.join(__dirname, 'api'));
 var db = require('./db');
-var notif = require('./notif');
-
 
 server.serve();
-notif.send('test of the system');
